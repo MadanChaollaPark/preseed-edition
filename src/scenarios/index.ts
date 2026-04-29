69aa861623abe6ba0630f3f97ce84e3f54a73724
Madan ChaollaPark <chaollapark@gmail.com>
2026-04-30 01:23:49 +0200

    Fork: jvnm-dev/pokemon @ HEAD as starting point

---
+++ src/scenarios/index.ts
@@ -0,0 +1,5 @@
+import scenario1 from "./001_oak";
+import scenario2 from "./002_scientist";
+import scenario3 from "./003_oak_lab_pokeball";
+
+export const scenarios = [scenario1, scenario2, scenario3];
